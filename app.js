c8c2bab2940148427b1d7b575685086314b288d8
Jay Palin <jaypalin101@gmail.com>
2016-08-19 13:20:32 -0700

    made csslint and jshint changes

--- app.js
+++ app.js
@@ -10,24 +10,34 @@ $(document).ready(function(){
         dataType: 'json',
         type: 'GET',
         success: function(data) {
-          if(searchQuery.length!=0){
-            $("#search-empty").removeClass("active");
-            $("#search-results").addClass("active");
-
+          if(searchQuery.length!==0){
             $("#result-list").html(""); // clear content
-
             var numResult = data[1].length;	// number of results
             var resultData = data[1]; // result data for checking purposes
-
+            if(numResult !== 0){
+              $('#numList').css('text-align', 'left');
+              $("#numList").html("Top " + numResult + " results");
+            }
+            else{
+              $('#numList').css('text-align', 'center');
+              $("#numList").html("No search reults found.");
+            }
             for(var i = 0; i<numResult;i++){ // for every result get each content
               $("#result-list").append('<li class="resultli"><a href="'+data[3][i]+'" target="_blank"><h3 class="result-header">'+data[1][i]+'</h3></a><p>'+data[2][i]+'</p></li>');
             }
-            $("#result-list").addClass("animated fadeInDown");
+          }
+          else {
+            $('#numList').css('text-align', 'center');
+            $('#numList').html('*Please type into the search bar');
           }
         },
         error: function(errorMessage){
           alert("Unable to connect");
         }
     });
+    $('#searchBox').val('');
 	});
+  $('#searchBox').keypress(function(e){
+    if(e.which === 13){$('#search-me').click();}
+  });
 });
